71fa67c3b1a12615b2644d499880d63c453c93f8
RaulSanabriaMarroquin <raulsanabria@estudianTEC.cr>
2023-10-17 04:25:25 -0600

    Conexion a bd ya funcional + Pagina crear eventos

--- LayoutTemplateWebApp/Data/ApplicationDbContext.cs
+++ LayoutTemplateWebApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 using LayoutTemplateWebApp.Model;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace LayoutTemplateWebApp.Data
 {
@@ -10,6 +12,27 @@ namespace LayoutTemplateWebApp.Data
         }
 
         public DbSet<Event> Event { get; set; } // DbSet para la entidad Event
-        // Agrega otros DbSet para las demás entidades de tu modelo aquí
-    }
+                                                // Agrega otros DbSet para las demás entidades de tu modelo aquí
+        public async Task CreateEventAsync(string name, DateTime date, int idEventState, string description, string organizer, string maxCapacity, string entryCost, int idEventType)
+        {
+            using (var connection = Database.GetDbConnection() as SqlConnection)
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+
+                using (var command = new SqlCommand("CreateEvent", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.Add(new SqlParameter("@name", name));
+                    command.Parameters.Add(new SqlParameter("@date", date));
+                    command.Parameters.Add(new SqlParameter("@idEventState", idEventState));
+                    command.Parameters.Add(new SqlParameter("@description", description));
+                    command.Parameters.Add(new SqlParameter("@organizer", organizer));
+                    command.Parameters.Add(new SqlParameter("@maxCapacity", maxCapacity));
+                    command.Parameters.Add(new SqlParameter("@entryCost", entryCost));
+                    command.Parameters.Add(new SqlParameter("@idEventType", idEventType));
+
+                    await command.ExecuteNonQueryAsync();
+                }
 }
